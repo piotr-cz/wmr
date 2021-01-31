fdf1410d6b18b292fcabf305d2b5e997db67ad08
jdecroock <decroockjovi@gmail.com>
2021-02-01 00:25:54 +0100

    undo changes in main app

--- packages/wmr/rollup.config.js
+++ packages/wmr/rollup.config.js
@@ -45,7 +45,7 @@ const config = {
 			}
 		]
 	},
-	external: [...builtins, '@swc/core'],
+	external: [...builtins],
 	// /* Logs all included npm dependencies: */
 	// external(source, importer) {
 	// 	const ch = source[0];
